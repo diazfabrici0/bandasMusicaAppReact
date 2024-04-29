2c4e4c3580df5be22e8ef05f7ca915e6c0332903
Jose Centena <jose.acentena@gmail.com>
2024-04-29 20:44:10 -0300

    commit react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
-
 import Home from './pages/home/home';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
-  );
+    <Router>
+      <div>
+        <Home />
+      </div>
+    </Router>
+  ); 
 }
 
 export default App;
+

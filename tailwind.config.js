f94514ffa4fceb15f16577bec24f1c6e9765ac86
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-05-01 17:21:22 -0300

    se agregaron fuentes y fondos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        Tilt: ["Tilt Prism", "cursive"],
+        Montserrat: ["Montserrat", "italic"],
+        Nunito: ["Nunito", "italic"],
+      },
+      backgroundImage: {
+        card: "url('/public/images/background_card.jpg')",
+        card_extra: "url('/public/images/background_card1.jpeg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};

4ce8dfbe6ced71c5b35f73d9859acc5a30cb0abb
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-05-01 17:31:05 -0300

    actualizacion de exportaciones

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export * from "./Button/Button";
 export * from "./Card/Card";
-export * from "./CardDetails/CardDetails";
 export * from "./Slider/Slider";
 export * from "./Subtitle/Subtitle";
 export * from "./Title/Title";

a2974fd2c34c341fdca5f92a7b2b3a54f8e5de4e
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-05-01 17:27:11 -0300

    archivos barril

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export * from "./Button/Button";
+export * from "./Card/Card";
+export * from "./CardDetails/CardDetails";
+export * from "./Slider/Slider";
+export * from "./Subtitle/Subtitle";
+export * from "./Title/Title";
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export * from "./Home/Home";
+export * from "./Galery/Galery";
+export * from "./Details/Details";

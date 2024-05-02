b3457d1f9b8b64282cf4239173b503740b4adf08
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-05-02 18:31:35 -0300

    se agrego pagina de error

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,8 @@
+export const Error = () => {
+  return (
+    <div className="flex flex-col mt-10 items-center justify-center">
+      <p className="font-Montserrat text-5xl mb-5">Ups! </p>
+      <p className="font-Montserrat text-4xl">Pagina no encontrada</p>
+    </div>
+  );
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Details, Galery, Home } from "../pages";
+import { Details, Error, Galery, Home } from "../pages";
 import { ROUTES } from "../const/routes";
 
 const router = createBrowserRouter([
   {
     path: ROUTES.home,
     element: <Home />,
+    errorElement: <Error />,
   },
   {
     path: ROUTES.galery,
     element: <Galery />,
+    errorElement: <Error />,
   },
   {
     path: ROUTES.details,
     element: <Details />,
+    errorElement: <Error />,
   },
 ]);
 

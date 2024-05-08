4c8b3f08936b97fdcd32be70676a475ded76f286
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-05-08 09:43:32 -0300

    se borraron comentarios

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,25 +3,6 @@ import { BandsRoutes } from "../routes/BandsRoutes";
 import { ROUTES } from "../const/routes";
 import { Details, Error, Galery, Home } from "../pages";
 
-// const router = createBrowserRouter([
-//   {
-//     path: ROUTES.home,
-//     element: <Home />,
-//   },
-//   {
-//     path: ROUTES.galery,
-//     element: <Galery />,
-//   },
-//   {
-//     path: ROUTES.details,
-//     element: <Details />,
-//   },
-//   {
-//     path: "*",
-//     element: <Error />,
-//   },
-// ]);
-
 const router = createBrowserRouter([
   {
     element: <BandsRoutes />,
